1e11da148d8f4e546101388796d7f658b4162049
Felix <65657867+generalfn27@users.noreply.github.com>
2024-12-25 17:51:45 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // gawan na to ng gui
+// manager check if nag exist pero jpanel dito alam ko
         System.out.println("Hello, World!");
     }
 }
