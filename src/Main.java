cb72e513ade6cb14bf72ceb9db47ce6460539795
Felix <generalfelix27@gmail.com>
2024-12-27 04:39:13 +0800

    oopr2 project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // gawan na to ng gui
-        // manager check if nag exist pero jpanel dito alam ko
+        /*
+         gawan na to ng gui
+         manager check if nag exist pero jpanel dito alam ko
+        */
         System.out.println("Hello, World!");
     }
 }
